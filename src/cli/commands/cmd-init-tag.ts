d6085373e0ba0a64f46ed0f893b3c52e9b200c5a
TheTonsOfCode <pawe9145@gmail.com>
2025-10-03 10:49:27 +0200

    cleaning

--- src/cli/commands/cmd-init-tag.ts
+++ src/cli/commands/cmd-init-tag.ts
@@ -2,11 +2,21 @@
 
 // Brac plik z mustache
 
+import {$LT_GetCommandEssentials} from "@/cli/commands/setup.ts";
+
 export async function $LT_CMD_InitTagFile() {
+    const {config, logger} = await $LT_GetCommandEssentials();
+
+
 
     // TODO: jesli typescript w projekcie to .ts, a jak nie ma to .js
     // TODO: nazwa taga to cos
     // TODO: brac pod uwage arg
     // TODO: if library to cos (np. InputType)
     // TODO: if react to useMemo
+
+    // Options
+    const tagName = config.tagName;
+    const isLibrary = config.isLibrary;
+    const isJS = false;
 }
