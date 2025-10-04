474a0db6e103f691e4071de37ed0f7560879beb0
TheTonsOfCode <pawe9145@gmail.com>
2025-10-04 10:03:41 +0200

    remove old transformer tests

--- tests/default-tag/_TODO_refactor/_shared-transformer.ts
+++
@@ -1,8 +0,0 @@
-
-import {LangTagTranslationsConfig, TranslationTransformer} from "@/index.ts";
-import {processPlaceholders} from "../vanilla-process-placeholders-fn.ts";
-
-
-export const defaultTranslationTransformer: TranslationTransformer<LangTagTranslationsConfig> = ({ value, params }) => {
-    return processPlaceholders(value, params);
-};
