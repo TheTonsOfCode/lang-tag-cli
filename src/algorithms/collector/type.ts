55bcb05d4ba5d54755ee0da2bc50360bd29c2738
TheTonsOfCode <pawe9145@gmail.com>
2025-10-24 20:28:02 +0200

    collectors todo

--- src/algorithms/collector/type.ts
+++ src/algorithms/collector/type.ts
@@ -18,19 +18,4 @@ export abstract class TranslationsCollector {
 
     abstract postWrite(changedCollections: string[]): Promise<void>;
 
-    // TODO: na
-    //  `Original namespace file "{namespace}.json" not found. A new one will be created.`, { namespace }
-
-    // TODO: dorobic ze ktos moze dac w globalnym configu 'treatNamespaceAsPathPrefix' aby bylo ladniej i nie polegamy wtedy na
-    //      appendNamespaceToPath w dictionary collectorze i jest ladniej (ale tą opcję też zostawić)
 }
-
-// w filters to trzeba bedzie wywalic:
-/*
-  if (!tag.parameterConfig.namespace) {
-            logger.warn('Skipping tag "{fullMatch}". Tag configuration namespace not defined. (Check lang-tag config at collect.onCollectConfigFix)', {
-                fullMatch: tag.fullMatch.trim()
-            });
-            return false;
-        }
- */
