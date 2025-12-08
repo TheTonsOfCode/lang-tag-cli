11c851eabc8381a64012c259599f5e2c03f877fa
TheTonsOfCode <pawe9145@gmail.com>
2025-12-08 20:19:21 +0100

    fix: core types compilation (#32)

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,3 +2,6 @@ export * from './types';
 export * from './create-callable-translations';
 export * from './flexible';
 export * from './lookup';
+
+// TODO: variables from string
+// TODO: value processor functions
